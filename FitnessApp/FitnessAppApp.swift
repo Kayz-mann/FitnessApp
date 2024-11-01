5c81a3aaf7c87c5c491d45f0129519c9fdac50a5
Kayode Balogun <kayodefredrickbalogun@gmail.com>
2024-11-01 15:21:42 +0100

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Balogun Kayode on 01/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
